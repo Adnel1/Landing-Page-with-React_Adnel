13a15a9e619494d831dd68270a0fc6fcb2973e2d
Ernesto Martinez <61639551+ErnestWarhead@users.noreply.github.com>
2024-03-11 17:56:03 +0000

    fixes

--- src/js/App.js
+++ src/js/App.js
@@ -1,18 +1,18 @@
 //import react into the bundle
-import React from "react";
 import React, { useState } from "react";
 
 //import your own components
 import Navbar from "./component/navbar.jsx";
 import Home from "./component/home.jsx";
 import Footer from "./component/footer.jsx";
+import CreativeMode from "./component/creativeMode.jsx";
 
 function App() {
     const [view, setView] = useState("home")
     return (
         <>
         <Navbar setView = {setView}/>
-        {setView === "home" ? <Home /> : <creativeMode />}
+        {view === "home" ? <Home /> : <CreativeMode />}
         <Footer />
         </>
     )
--- src/js/component/creativeMode.jsx
+++ src/js/component/creativeMode.jsx
@@ -1,6 +1,6 @@
 import React, { useState  } from "react";
 
-const creativeMode = () => {
+const CreativeMode = () => {
 
 const [imgLink, setImgLink] = useState("");
 const [title, setTitle] = useState("");
@@ -28,8 +28,8 @@ const cardDelete = (index) => setCardsInfo([...cardsInfo.slice(0, index), ...car
 
 
 return (
-<>
-    <div className="d-flex flex-wrap justify-content-between">
+<div style={{ width: "80%", margin: "0 auto" }}>
+    <div className="d-flex flex-wrap">
     {
     (cardsInfo.length !== 0 ? cardsInfo : [{
         imgLink: "https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_52683-60691.jpg?w=1380&t=st=1710166958~exp=1710167558~hmac=806861196ca76ba90c435d0aee12f53906c795f3a3eb36da99be524a72b2c950",
@@ -37,7 +37,7 @@ return (
         content: "Card Content",
         link: "#"
     }]).map((card, index) => (
-        <div key={index} className="card text-center width-250 mb-4">
+        <div key={index} className="card text-center width-250 m-3">
             <img src={card.imgLink || "https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_52683-60691.jpg?w=1380&t=st=1710166958~exp=1710167558~hmac=806861196ca76ba90c435d0aee12f53906c795f3a3eb36da99be524a72b2c950"} className="card-img-top" />
             <div className="card-body">
                 <h5 className="card-title">{card.title}</h5>
@@ -45,36 +45,35 @@ return (
             </div>
             <div className="card-footer bg-light-grey">
                 <a href={card.link || "#"} className="btn btn-primary">Find Out More!</a>
-                <a href="#" onClick={() => cardDelete(index)} className="btn btn-danger">X</a>
+                <a href="#" onClick={() => cardDelete(index)} className="btn btn-danger m-2">X</a>
             </div>
         </div>
     )/*close function*/
     )/*close .map*/
     }   
     </div>
-
-
-
+ 
     <form onSubmit={cardAdd}>
-    <div className="input-group mb-3">
+    <div className="input-group m-3">
         <span className="input-group-text" id="basic-addon1">Card image link</span>
             <input type="text" value={imgLink} onChange={(e) => setImgLink(e.target.value)} className="form-control" placeholder="You may leave this one empty" aria-label="Card image link" aria-describedby="basic-addon1" />
         </div>
-            <div className="input-group mb-3">
+            <div className="input-group m-3">
             <span className="input-group-text" id="basic-addon1">Set Card Title</span>
             <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} className="form-control" required placeholder="Set Card Title" aria-label="Card Title" aria-describedby="basic-addon1" />
         </div>
-            <div className="input-group mb-3">
+            <div className="input-group m-3">
             <span className="input-group-text" id="basic-addon1">Set Card Content</span>
             <input type="text" value={content} onChange={(e) => setContent(e.target.value)} className="form-control" required placeholder="Set Card Content" aria-label="Card Content" aria-describedby="basic-addon1" />
         </div>
-            <div className="input-group mb-3">
+            <div className="input-group m-3">
             <span className="input-group-text" id="basic-addon1">Find out more! link</span>
             <input type="text" value={link} onChange={(e) => setLink(e.target.value)} className="form-control" placeholder="You may leave this one empty" aria-label="Card image link" aria-describedby="basic-addon1" />
-            <button className="btn btn-primary" type="submit" value="submit">Button</button>
         </div>
+        <button className="btn btn-primary m-3" type="submit" value="submit">Submit</button>
         </form>
-</>
+
+</div>
 )/*close return*/};
 
-export default creativeMode;
+export default CreativeMode;
